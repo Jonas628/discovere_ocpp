85df09f129976705addb2beb726346f6e7b714f9
Ole Bialas <bialas.ole@gmx.de>
2020-12-15 11:49:48 +0100

    removing all features except charging station and central system

---
+++ src/central_system.py
@@ -0,0 +1,63 @@
+import asyncio
+import websockets
+from datetime import datetime
+from pathlib import Path
+from ocpp.routing import on
+from ocpp.v16 import ChargePoint as Cp
+from ocpp.v16.enums import Action, RegistrationStatus
+from ocpp.v16 import call_result
+import mongoengine
+
+DATADIR = Path("/home/ole/projects/charging_management/data/")
+mongoengine.register_connection(alias='core', name='OCPP')
+
+
+class MyChargePoint(Cp):
+    """
+    Make an instance of this everytime a charge_point is connected.
+    """
+    @on(Action.BootNotification)
+    def on_boot_notification(self, charge_point_vendor, charge_point_model, **kwargs):
+        print("ChargePoint Connected!")
+        return call_result.BootNotificationPayload(
+            current_time=str(datetime.utcnow()),
+            interval=10,
+            status=RegistrationStatus.accepted)
+
+    @on(Action.Heartbeat)
+    async def on_heartbeat(self):
+        print(f"received Heartbeat at: {datetime.utcnow()}")
+        return call_result.HeartbeatPayload(current_time=str(datetime.utcnow()))
+
+    @on(Action.MeterValues)
+    async def on_meter_value(self, connector_id, transaction_id, meter_value):
+        print(meter_value)
+        # svc.write_meter_value(meter_value[0])
+        return call_result.MeterValuesPayload()
+
+
+class CentralSystem(object):
+
+    def __init__(self):
+        self.clients = []  # all currently connected charging_points
+
+    async def register(self, websocket):
+        charge_point_id = "CP01"
+        self.clients.append(MyChargePoint(charge_point_id, websocket))
+        await asyncio.gather(self.clients[-1].start())
+
+    async def ws_handler(self, websocket, uri):
+        await self.register(websocket)  # add the client to the list of clients
+
+
+if __name__ == '__main__':
+    server = CentralSystem()
+    # Whenever a client connects, the server accepts the connection,
+    # creates a WebSocketServerProtocol, performs the opening handshake,
+    # and delegates to the connection handler defined by ws_handler.
+    # Once the handler completes, either normally or with an exception,
+    # the server performs the closing handshake and closes the connection
+    start_server = websockets.serve(server.ws_handler, "localhost", 3000)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_server)
+    loop.run_forever()
---
+++ src/charging_station.py
@@ -0,0 +1,69 @@
+import asyncio
+import websockets
+from datetime import datetime
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16 import call
+from ocpp.v16.enums import RegistrationStatus
+
+
+class ChargePoint(cp):
+    """
+    Charge point class which inherits from the OCPP python module's charge
+    point. The parent class handles the sending and receiving of messages.
+    This class adds some methods for sending messages and executing functions
+    when a specific message is received
+    """
+
+    def __init__(self, id, connection, response_timeout=30):
+        super().__init__(id, connection, response_timeout=response_timeout)
+        self.maxpower = float(11)
+
+    async def send_boot_notification(self):
+        request = call.BootNotificationPayload(
+            charge_point_model="Weeyu",
+            charge_point_vendor="Discovere"
+        )
+        response = await self.call(request)
+        if response.status == RegistrationStatus.accepted:
+            print("Connected to central system.")
+
+    async def send_heartbeats(self):
+        while True:
+            request = call.HeartbeatPayload()
+            await self.call(request)
+            await asyncio.sleep(10)
+
+    async def send_meter_value(self, power, cid=1, tid=1):
+        request = call.MeterValuesPayload(
+            connector_id=cid,
+            transaction_id=tid,
+            meter_value=[{
+                "timestamp": str(datetime.utcnow()),
+                "sampledValue": [
+                    {"measurand": "Power.Active.Import",
+                     "phase": "N",
+                     "unit": "kW",
+                     "value": str(power)},
+                    {"measurand": "Energy.Active.Import.Register",
+                     "phase": "N",
+                     "unit": "kWh",
+                     "value": "0.00"}]}]
+        )
+        await self.call(request)
+
+
+async def main(hostname: str, port: int) -> None:
+    websocket_resource_url = f"ws://{hostname}:{port}"
+    # open the connection with a websocket
+    async with websockets.connect(websocket_resource_url) as websocket:
+        cp = ChargePoint('CP_1', websocket)
+        # when the charge point is started it is waiting for messages
+        await asyncio.gather(cp.start(), cp.send_boot_notification(), cp.send_heartbeats())
+
+
+if __name__ == '__main__':
+    # specify host and port and run forever, will raise an error if the
+    # server is not found
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(hostname='localhost', port=3000))
+    loop.run_forever()
